29eec03e5ff20aa82126dbb74cd2cd8dcfb460b9
innamagkeeva <inna.m79@mail.ru>
2025-05-03 02:55:49 +0300

    добавление задачи.изменение статуса и удаление задачи

--- src/App.vue
+++ src/App.vue
@@ -2,37 +2,66 @@
 import { ref } from 'vue'
 //создаю модель инпута высокого и низкого. изначально она пустая строка.
 
-const inputTaskTextHigh = ref<string>('') //не знаю, тут надо типизировать?????!!!!!!!
+const inputTaskTextHigh = ref('')
 const inputTaskTextLow = ref('')
 
 // так как список задач - это массив - то нужно создать константу в которой будет пустой массив, в него будут пушиться задачи при нажатии на +
-const tasksTextHigh = ref<string[]>([])
-const tasksTextLow = ref<string[]>([])
+// const tasksTextHigh = ref<string[]>([])
+// const tasksTextLow = ref<string[]>([])
+
+//надо создать массив объектов с тремя полями: задача, статус(сделано/не сделано) и приоритет (высокий/низкий)
+
+interface Task {
+  text: string
+  status: 'done' | 'notDone'
+  priority: 'high' | 'low'
+}
+
+const tasks = ref<Task[]>([])
+
+// function addTask(): void {
+//   const textHigh = inputTaskTextHigh.value.trim()
+//   const textLow = inputTaskTextLow.value.trim()
+//   if (textHigh) {
+//     const capitalizedHighText = textHigh[0].toUpperCase()+textHigh.slice(1)
+//     tasksText.value.push({ text: capitalizedHighText, status: 'notDone', priority: 'high' })
+//     inputTaskTextHigh.value = ''
+//   }
+// if (textLow) {
+//   const capitalizedLowText = textLow[0].toUpperCase()+textLow.slice(1)
+//   tasksText.value.push({ text: capitalizedLowText, status: 'notDone', priority: 'low' })
+//   inputTaskTextLow.value = ''
+// }
+// }
 
 function addTaskHigh(): void {
-  const text = inputTaskTextHigh.value.trim()
-  if (text) {
-    const capitalizedText = text.charAt(0).toUpperCase() + text.slice(1)
-    tasksTextHigh.value.push(capitalizedText)
+  const textHigh = inputTaskTextHigh.value.trim()
+  if (textHigh) {
+    const capitalizedHighText = textHigh[0].toUpperCase() + textHigh.slice(1)
+    tasks.value.push({ text: capitalizedHighText, status: 'notDone', priority: 'high' })
     inputTaskTextHigh.value = ''
   }
 }
 
 function addTaskLow(): void {
-  const text = inputTaskTextLow.value.trim()
-  if (text) {
-    const capitalizedText = text.charAt(0).toUpperCase() + text.slice(1)
-    tasksTextLow.value.push(capitalizedText)
+  const textLow = inputTaskTextLow.value.trim()
+  if (textLow) {
+    const capitalizedLowText = textLow[0].toUpperCase() + textLow.slice(1)
+    tasks.value.push({ text: capitalizedLowText, status: 'notDone', priority: 'low' })
     inputTaskTextLow.value = ''
   }
 }
 
-function deleteTaskHigh(index: number): void {
-  tasksTextHigh.value.splice(index, 1)
-}
+function changeStatus(task: Task): void {
+  task.status = task.status === 'done' ? 'notDone' : 'done'
+} // task.status === 'done' это true. если в задачи 'notDone' то это false и когда функция сработает, то 'notDone' поменяется на 'done'
 
-function deleteTaskLow(index: number): void {
-  tasksTextLow.value.splice(index, 1)
+function deleteTask(taskToDelete: Task): void {
+  //тип Task потому что taskToDelete-объект.
+  const index = tasks.value.findIndex((task) => task === taskToDelete)
+  if (index !== -1) {
+    tasks.value.splice(index, 1)
+  }
 }
 </script>
 
@@ -40,7 +69,10 @@ function deleteTaskLow(index: number): void {
   <div class="tasks">
     <h1 class="title">HIGH</h1>
     <!-- при нажатии на + сработала функция и текст из инпута попал в массив задач, но мне нужно чтоб создался новый ли для каждой задачи.Для этого я на li вешаю директиву vi-for которая будет проходиться по каждой задачи из массива  -->
-    <form class="form" @submit.prevent="addTaskHigh">
+    <form
+      class="form"
+      @submit.prevent="addTaskHigh"
+    >
       <input
         class="form__input"
         type="text"
@@ -49,18 +81,40 @@ function deleteTaskLow(index: number): void {
         placeholder="Добавить важных дел"
         v-model="inputTaskTextHigh"
       />
-      <button class="form__button" type="submit">+</button>
+      <button
+        class="form__button"
+        type="submit"
+      >
+        +
+      </button>
     </form>
     <ul class="list">
-      <li class="list__item" v-for="(task, index) in tasksTextHigh" :key="task">
-        <input class="list__checkbox" type="checkbox" />
-        <p class="list__text">{{ task }}</p>
-        <button class="list__button-delete" @click="deleteTaskHigh(index)">+</button>
+      <li
+        class="list__item"
+        v-for="task in tasks.filter((task) => task.priority === 'high')"
+        :key="task.text"
+      >
+        <input
+          class="list__checkbox"
+          type="checkbox"
+          @click="changeStatus(task)"
+          :checked="task.status === 'done'"
+        />
+        <p class="list__text">{{ task.text }}</p>
+        <button
+          class="list__button-delete"
+          @click="deleteTask(task)"
+        >
+          +
+        </button>
       </li>
     </ul>
 
     <h1 class="title">LOW</h1>
-    <form class="form" @submit.prevent="addTaskLow">
+    <form
+      class="form"
+      @submit.prevent="addTaskLow"
+    >
       <input
         class="form__input"
         type="text"
@@ -69,13 +123,32 @@ function deleteTaskLow(index: number): void {
         placeholder="Не очень важные дела"
         v-model="inputTaskTextLow"
       />
-      <button class="form__button" type="submit">+</button>
+      <button
+        class="form__button"
+        type="submit"
+      >
+        +
+      </button>
     </form>
     <ul class="list">
-      <li class="list__item" v-for="(task, index) in tasksTextLow" :key="task">
-        <input class="list__checkbox" type="checkbox" />
-        <p class="list__text">{{ task }}</p>
-        <button class="list__button-delete" @click="deleteTaskLow(index)">+</button>
+      <li
+        class="list__item"
+        v-for="task in tasks.filter((task) => task.priority === 'low')"
+        :key="task.text"
+      >
+        <input
+          class="list__checkbox"
+          type="checkbox"
+          @click="changeStatus(task)"
+          :checked="task.status === 'done'"
+        />
+        <p class="list__text">{{ task.text }}</p>
+        <button
+          class="list__button-delete"
+          @click="deleteTask(task)"
+        >
+          +
+        </button>
       </li>
     </ul>
   </div>
@@ -148,6 +221,15 @@ body {
   width: 20px;
   height: 20px;
   margin-right: 10px;
+  appearance: none;
+  border: 1px solid black;
+  border-radius: 50%;
+  padding: 3px;
+}
+
+.list__checkbox:checked {
+  background-color: #ccc;
+  background-clip: content-box;
 }
 
 .list__text {
